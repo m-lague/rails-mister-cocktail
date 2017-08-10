0e730aaafb6ac9495a531bb48cd145e3c4b85aa8
Mathieu Lague <mathieu.lague@laposte.net>
2017-08-10 16:15:23 +0200

    generate doses & cocktails controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'doses/new'
+
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    # collection do
+    #   resources :doses, only: [:new, :create]
+    # end
+
+    resources :doses, only: [:new, :create]
+
+
+  end
+  resources :doses, only: [:destroy]
+
 end
