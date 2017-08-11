65f515dc8d997a62997653ed8127340b9a62ebe9
Mathieu Lague <mathieu.lague@laposte.net>
2017-08-11 12:49:20 +0200

    new seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,95 @@
 Cocktail.destroy_all
 Ingredient.destroy_all
 
-lemon = Ingredient.create(name: "lemon")
-ice = Ingredient.create(name: "ice")
-mint = Ingredient.create(name: "mint leaves")
+cocktails = [
+  {
+    name: "Old Fashioned",
+    picture: "http://www.seriouseats.com/images/2014/11/20141104-cocktail-party-old-fashioneds-holiday-vicky-wasik-3.jpg"
+    },
+  {
+    name: "Daiquiri",
+    picture: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-daiquiri.jpg"
+    },
+  {
+    name: "Margarita",
+    picture: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-margarita.jpg"
+    },
+  {
+    name: "Sidecar",
+    picture: "http://www.seriouseats.com/images/2014/11/20141101-cognac-sidecar-carey-jones.jpg"
+    },
+  {
+    name: "French 75",
+    picture: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-french75.jpg"
+    },
+  {
+    name: "Bloody Mary",
+    picture: "http://www.seriouseats.com/images/2015/03/twase-20150320-21.jpg"
+    },
+  {
+    name: "Irish Coffee",
+    picture: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-irish-coffee.jpg"
+    },
+  {
+    name: "Jack Rose",
+    picture: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-jack-rose.jpg"
+    },
+  {
+    name: "Negroni",
+    picture: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-negroni.jpg"
+    },
+  {
+    name: "Boulevardier",
+    picture: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-boulevardier.jpg"
+    },
+  {
+    name: "Sazerac",
+    picture: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-sazerac.jpg"
+    },
+  {
+    name: "Vieux Carré",
+    picture: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-vieux-carre.jpg"
+    },
+  {
+    name: "Ramos Gin Fizz",
+    picture: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-robyn-lee-ramos-gin-fizz.jpg"
+    },
+  {
+    name: "Mint Julep",
+    picture: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-mint-julep.jpg"
+    },
+  {
+    name: "Whiskey Sour",
+    picture: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-whiskey-sour.jpg"
+    },
+  {
+    name: "Mai Tai",
+    picture: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-mai-tai.jpg"
+    },
+  {
+    name: "Planter's Punch",
+    picture: "http://www.seriouseats.com/images/2015/04/20150406-cocktails-planters-punch-robyn-lee-1.jpg"
+    },
+  {
+    name: "Pisco Sour",
+    picture: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-pisco-sour.jpg"
+    },
+  {
+    name: "Cosmopolitan",
+    picture: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-cosmopolitan.jpg"
+    },
+  {
+    name: "Tom Collins",
+    picture: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-tom-collins.jpg"
+    },
+  {
+    name: "Last Word",
+    picture: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-last-word.jpg"
+  }
+]
 
-mojito = Cocktail.create(name: "mojito")
-sunrise = Cocktail.create(name: "tekila sunrise")
-blue_lagoon = Cocktail.create(name: "blue lagoon")
-bloody_mary = Cocktail.create(name: "bloody mary")
+ingredients = %w(lemon ice mint leaves redbull jagermeister sugar tonic gin rhum)
+ingredients.each { |ingredient| Ingredient.create(name: ingredient) }
 
 
-Dose.create!(description: "2 lemons", cocktail: mojito, ingredient: lemon )
-Dose.create!(description: "1 cl of lemon", cocktail: sunrise, ingredient: lemon )
-Dose.create!(description: "4 ice", cocktail: blue_lagoon, ingredient: ice )
-Dose.create!(description: "2cl of mint", cocktail: blue_lagoon, ingredient: mint )
-Dose.create!(description: "5 ices", cocktail: bloody_mary, ingredient: ice )
-Dose.create!(description: "1 lemon", cocktail: bloody_mary, ingredient: lemon)
+cocktails.each { |cocktail| Cocktail.create(cocktail) }
