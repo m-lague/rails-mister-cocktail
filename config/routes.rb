a97961f5587f50315bffea0f07ab6f0402754237
Mathieu Lague <mathieu.lague@laposte.net>
2017-08-11 14:45:06 +0200

    reparation cocktails#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "cocktails#index"
-  resources :cocktails, only: [:show, :new, :create], shallow: true do
+  resources :cocktails, only: [:index, :show, :new, :create], shallow: true do
     resources :doses, only: [:create, :destroy]
   end
 end
